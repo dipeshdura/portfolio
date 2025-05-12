4f6743bb6b457e794fdf40c21074067f88371480
dipeshdura <dipeshdura14@gmail.com>
2025-05-12 11:30:42 +0545

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:"0.0.0.0",
-    port:5173,
-    proxy:{
-      "/api":{
-        target:"https://portfolio-scx5.onrender.com",
-        secure:false
-      }
-    }
-  }
-})
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://portfolio-scx5.onrender.com",
+        secure: false,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
