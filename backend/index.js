2786a863f4bd5e29a891ef66d11ec468d1e36102
dipeshdura <dipeshdura14@gmail.com>
2025-05-22 20:11:14 +0545

    index middleware

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,15 @@ app.use(express.json());
 app.use(cors());
 app.use("/api",contactRoute);
 
-
+app.use((err,_,res,next)=>{
+    const statusCode =err.statusCode || 500;
+    const message =err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode
+    })
+})
 
 
 
