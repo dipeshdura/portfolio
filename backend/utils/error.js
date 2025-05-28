494f5965c78f24f4db3d861a6e621497aebbb06e
dipeshdura <dipeshdura14@gmail.com>
2025-05-28 08:59:32 +0545

    error functionality

--- backend/utils/error.js
+++ backend/utils/error.js
@@ -2,7 +2,7 @@ export const asyncHandler =(fn)=>(req,res,next)=>{
     Promise.resolve(fn(req,res,next)).catch(next);
 }
 
-export const errorHandler=(statusCode, message){
+export const errorHandler=(statusCode, message)=>{
     const error =new Error();
     error.statusCode =statusCode;
     error.message =message;
